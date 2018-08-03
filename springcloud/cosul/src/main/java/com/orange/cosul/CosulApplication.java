40e1828c743d4c2fc633a9929ace42a2e81a336e
genghj <8588gz@163.com>
2018-08-03 18:23:58 +0800

    consul

---
+++ springcloud/cosul/src/main/java/com/orange/cosul/CosulApplication.java
@@ -0,0 +1,20 @@
+package com.orange.cosul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@RestController
+@SpringBootApplication
+public class CosulApplication {
+	@RequestMapping("/hi")
+	public String home() {
+		return "hi ,i'm miya";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CosulApplication.class, args);
+	}
+}
---
+++ springcloud/cosul/src/test/java/com/orange/cosul/CosulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orange.cosul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CosulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
